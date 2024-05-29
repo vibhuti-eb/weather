a8d141f75779bce5931bfb5067d3223688460234
vibhuti-eb <vibhuti@eventbrite.com>
2024-05-29 16:00:20 +0530

    Creating DBs to keep records

--- bodyapp/models.py
+++ bodyapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CityRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=100)
+
+class WeatherResponse(models.Model):
+    cityRequest = models.OneToOneField(
+        CityRequest,
+        on_delete=models.PROTECT,
+        primary_key=True
+    )
+    longitude = models.CharField(max_length=10)
+    latitude = models.CharField(max_length=10)
+    temperature = models.CharField(max_length=5)
+    pressure = models.CharField(max_length=10)
+    humidity = models.CharField(max_length=10)
--- bodyapp/views.py
+++ bodyapp/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 import json, urllib.request
 from django.views.decorators.csrf import csrf_exempt
+from .models import CityRequest, WeatherResponse
 
 # Create your views here.
 
 @csrf_exempt
 def index(request):
-    # return HttpResponse("Hello")
-
     if request.method == 'GET':
         city = request.GET.get('city')
+        cityRequest = CityRequest(city = city)
+        cityRequest.save()
 
         weatherLoad = urllib.request.urlopen(
             'http://api.openweathermap.org/data/2.5/weather?q=' 
@@ -19,12 +20,22 @@ def index(request):
         weatherDict = json.loads(weatherLoad)
 
         weather = {
-            "long": str(weatherDict['coord']['lon']),
-            "lat" : str(weatherDict['coord']['lat']), 
-            "temp": str(round(weatherDict['main']['temp'] -  273.15, 1) )+ '\xb0C', 
+            "longitude": str(weatherDict['coord']['lon']),
+            "latitude" : str(weatherDict['coord']['lat']), 
+            "temperature": str(round(weatherDict['main']['temp'] -  273.15, 1) )+ '\xb0C', 
             "pressure": str(weatherDict['main']['pressure']), 
             "humidity": str(weatherDict['main']['humidity']), 
         }
+
+        weatherResponse = WeatherResponse(
+            cityRequest = cityRequest,
+            longitude = weather.get("longitude"),
+            latitude = weather.get("latitude"),
+            temperature = weather.get("temperature"),
+            pressure = weather.get("pressure"),
+            humidity = weather.get("humidity")
+        )
+        weatherResponse.save()
     
     else:
         weather={}
